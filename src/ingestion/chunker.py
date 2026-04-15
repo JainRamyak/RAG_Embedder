ca1ea51c5ca672930713e05906a4dcd975f8ba3c
Ramyak Jain <jain.ramyak0987@gmail.com>
2026-04-15 23:45:08 +0530

    Phase II Initiated

--- src/ingestion/chunker.py
+++ src/ingestion/chunker.py
@@ -1,5 +1,5 @@
 import logging
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from config import settings
 
 logger = logging.getLogger(__name__)
